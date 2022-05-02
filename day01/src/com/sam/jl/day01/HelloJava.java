7e239bbc0c3f1318c028b2acd23ec26a11b03cef
limq <limq@cgnfex.com>
2022-05-02 13:30:22 +0800

    fc

---
+++ day01/src/com/sam/jl/day01/HelloJava.java
@@ -0,0 +1,8 @@
+package com.sam.jl.day01;
+
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
---
+++ day01/src/com/sam/jl/day01/HelloThread.java
@@ -0,0 +1,8 @@
+package com.sam.jl.day01;
+
+/**
+ * @author limq
+ * @create 2022-05-02 9:18 上午
+ */
+public class HelloThread {
+}
